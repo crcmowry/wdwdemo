cc4351017d4b377be10e68164d160d6779da3fed
Connor Mowry <crcmowry@gmail.com>
2018-11-04 10:23:28 -0500

    Step 2: Jinja2 templating with HTML

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    post = "This is my first post!"
+    return render_template("index.html", post=post)
